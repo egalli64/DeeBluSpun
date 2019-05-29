89a67c0231965145b2827d75bab8c5d82feeb277
Piero <p.bocchinfuso@gmail.com>
2019-05-29 14:38:10 +0100

    pablo

--- Script/setup.sql
+++ Script/setup.sql
@@ -1,5 +1,5 @@
-DROP TABLE ristoranti;
-create table ristoranti (
+DROP TABLE bluristoranti;
+create table bluristoranti (
 ristoranti_id integer primary key,
 name varchar2(30),
 posizione varchar2(30),
@@ -7,11 +7,11 @@ specialita varchar2(10),
 recensione integer);
 
 
-INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (1, 'Primo Fiore', 'Taurianova', 'carne', 3);
-INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (2, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce', 5);
-INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (3, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce', 5);
-INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (4, 'Caposperone', 'Tonnara Di Palmi', 'pesce', 5);
