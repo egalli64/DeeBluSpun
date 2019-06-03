43d90e319a292ed3b68bbd0b55bdd3568bf4f518
Manuel <ciervomanuel@gmail>
2019-06-03 11:04:00 +0200

    modifica

--- src/main/java/dd/RistorantiCtrl.java
+++ src/main/java/dd/RistorantiCtrl.java
@@ -3,17 +3,19 @@ package dd;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class RistorantiCtrl {
 	private static final Logger logger = LoggerFactory.getLogger(RistorantiCtrl.class);
 	@Autowired
 	RistorantiRepo repo;
 
 	@GetMapping("/tabellaRistoranti")
-	public String regions(Model model) {
+	public String ristoranti(Model model) {
+		logger.trace("enter");
 		model.addAttribute("ristoranti", repo.findAll());
 		return "/tabellaRistoranti";
 	}
