0d41817a6bf49e874d8f836afa14adbd3e69c8b7
Domenica <domenic.furfaro89@gmail.com>
2019-06-03 10:28:03 +0200

    ristoranti

--- Script/setup.sql
+++ Script/setup.sql
@@ -9,3 +9,7 @@ INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (1, 'Primo Fiore', 'Taurianova', 'carne', '3Stelle');
 INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
 values (2, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce', '5Stelle');
+INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+values (3, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce', '5Stelle');
+INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
+values (4, 'Caposperone', 'Tonnara Di Palmi', 'pesce', '5Stelle');
