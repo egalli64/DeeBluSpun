3f93f5524fd4bda4ec8211c6b234d4c03a5eba95
Manuel <ciervomanuel@gmail>
2019-06-03 10:50:06 +0200

    sql manuel

--- Script/setup.sql
+++ Script/setup.sql
@@ -3,14 +3,15 @@ create table ristoranti (
 ristoranti_id integer primary key,
 name varchar2(30),
 posizione varchar2(30),
-specialita varchar2(2),
-recensione varchar(7));
+specialita varchar2(10),
+recensione integer);
+
 
 INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (1, 'Primo Fiore', 'Taurianova', 'carne', '3Stelle');
+values (1, 'Primo Fiore', 'Taurianova', 'carne', 3);
 INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (2, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce', '5Stelle');
+values (2, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce', 5);
 INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (3, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce', '5Stelle');
+values (3, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce', 5);
 INSERT INTO ristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (4, 'Caposperone', 'Tonnara Di Palmi', 'pesce', '5Stelle');
+values (4, 'Caposperone', 'Tonnara Di Palmi', 'pesce', 5);
