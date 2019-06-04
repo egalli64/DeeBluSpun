35d5078219903c936436a5431bca7d32b595a023
Domenica <domenic.furfaro89@gmail.com>
2019-06-04 10:27:42 +0200

    nuovo script

--- Script/setup.sql
+++ Script/setup.sql
@@ -7,14 +7,26 @@ ristoranti_id integer primary key,
 name varchar2(30),
 posizione varchar2(30),
 specialita varchar2(10),
-recensione integer);
+);
 
 
-INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (RISTORANTI_SEQ.nextval, 'Primo Fiore', 'Taurianova', 'carne', 3);
-INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (RISTORANTI_SEQ.nextval, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce', 5);
-INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (RISTORANTI_SEQ.nextval, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce', 5);
-INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita, recensione)
-values (RISTORANTI_SEQ.nextval, 'Caposperone', 'Tonnara Di Palmi', 'pesce', 5);
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
+values (RISTORANTI_SEQ.nextval, 'Primo Fiore', 'Taurianova', 'carne');
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
+values (RISTORANTI_SEQ.nextval, 'Il Parco Dei Principi', 'Gioiosa Ionica', 'pesce');
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
+values (RISTORANTI_SEQ.nextval, 'Il Castello Degli Dei', 'San Giorgio Morgeto', 'pesce');
+INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
+values (RISTORANTI_SEQ.nextval, 'Caposperone', 'Tonnara Di Palmi', 'pesce');
+
+
+drop sequence RECENSIONI_SEQ;
+create sequence RECENSIONI_SEQ;
+
+drop table blurecensioni;
+create table blurecensioni (
+recensioni_id integer primary key,
+ristoranti_id integer constraint recensioni_ristoranti_id_fk references bluristoranti(ristoranti_id) on delete cascade,
+stelle integer constraint recensioni_stelle_ck check (stelle between 1 and 5),
+);
+
