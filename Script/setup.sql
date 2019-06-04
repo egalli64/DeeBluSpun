efdb8ad92555fe9687afb538c32665b38a7d53b0
Domenica <domenic.furfaro89@gmail.com>
2019-06-04 11:04:54 +0200

    script finale

--- Script/setup.sql
+++ Script/setup.sql
@@ -1,13 +1,16 @@
 drop sequence RISTORANTI_SEQ;
-create sequence RISTORANTI_SEQ;
-
+drop sequence RECENSIONI_SEQ;
+drop table blurecensioni;
 DROP TABLE bluristoranti;
+create sequence RISTORANTI_SEQ;
+create sequence RECENSIONI_SEQ;
+
+
 create table bluristoranti (
 ristoranti_id integer primary key,
 name varchar2(30),
 posizione varchar2(30),
 specialita varchar2(10));
-
 
 
 INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
@@ -20,22 +23,19 @@ INSERT INTO bluristoranti(ristoranti_id, name, posizione, specialita)
 values (RISTORANTI_SEQ.nextval, 'Caposperone', 'Tonnara Di Palmi', 'pesce');
 
 
-drop sequence RECENSIONI_SEQ;
-create sequence RECENSIONI_SEQ;
-
-drop table blurecensioni;
 create table blurecensioni (
 recensioni_id integer primary key,
 ristoranti_id integer constraint recensioni_ristoranti_id_fk references bluristoranti(ristoranti_id) on delete cascade,
-stelle integer constraint recensioni_stelle_ck check (stelle between 1 and 5));
+stelle integer constraint recensioni_stelle_ck check (stelle between 1 and 5),
+utenti_id integer );
 
 
-INSERT INTO blurecensioni(recensioni_id ,ristoranti_id, stelle)
-values (recensioni_SEQ.nextval, 1, 4);
-INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle)
-values (recensioni_SEQ.nextval, 2, 5);
-INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle)
-values (recensioni_SEQ.nextval, 3, 3);
-INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle)
-values (recensioni_SEQ.nextval, 4, 1);
+INSERT INTO blurecensioni(recensioni_id ,ristoranti_id, stelle,utenti_id)
+values (recensioni_SEQ.nextval, 1, 4,1);
+INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle,utenti_id)
+values (recensioni_SEQ.nextval, 2, 5,2);
+INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle,utenti_id)
+values (recensioni_SEQ.nextval, 3, 3,3);
+INSERT INTO blurecensioni(recensioni_id, ristoranti_id, stelle,utenti_id)
+values (recensioni_SEQ.nextval, 4, 1,4);
 
